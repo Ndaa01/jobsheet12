441fcd5c4d5f3a254f9ed70e1c1239f0542f0fda
Ndaa01 <ananda.idn12345@gmail.com>
2023-11-20 13:25:45 +0700

    Percobaan 1 Pertanyaan 2

--- TerimaKasih05.java
+++ TerimaKasih05.java
@@ -3,7 +3,4 @@ public class TerimaKasih05 {
         System.out.println("Thank you for being the best teacher in the world.\n"+
         "You inspired in me a love for you learning and made me feel like I could ask yoou anything.");
     }
-    public static void Ucap(String[] args) {
-        
-    }
 }
