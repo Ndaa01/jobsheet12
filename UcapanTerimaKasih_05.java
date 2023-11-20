498165d9be4436a388372bd98fa016b07c618912
Ndaa01 <ananda.idn12345@gmail.com>
2023-11-20 14:21:18 +0700

    Percobaan 4 Pertanyaan 3

--- UcapanTerimaKasih_05.java
+++ UcapanTerimaKasih_05.java
@@ -13,7 +13,12 @@ public class UcapanTerimaKasih_05 {
         System.out.println("Thank you "+nama+" for being the best teacher in the world.\n"+
         "You inspired in me a love for you learning and made me feel like I could ask yoou anything."); 
     }
+    public static void UcapanTambahan(String ucapan) {
+        System.out.println(ucapan);
+    }
     public static void main(String[] args) {
         UcapanTerimaKasih();
+        String ucapan = "Terimakasih.. Semoga sehat selalu.";
+        UcapanTambahan(ucapan);
     }
 }
